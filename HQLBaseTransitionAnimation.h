25a6aab6f4cfbd09b8f135680d2573dc5853a32d
Heqiliang <heqiliang@weplus.cn>
2017-03-04 18:29:42 +0800

    commit

--- HQLBaseTransitionAnimation.h
+++ HQLBaseTransitionAnimation.h
@@ -2,7 +2,7 @@
 //  HQLBaseTransitionAnimation.h
 //  HQLTransitionAnimation
 //
-//  Created by weplus on 2017/2/16.
+//  Created by weplus on 2017/2/16.updateInteractiveTransition
 //  Copyright © 2017年 weplus. All rights reserved.
 //
 
